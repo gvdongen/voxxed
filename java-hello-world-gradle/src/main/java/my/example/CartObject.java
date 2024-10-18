55257433709fac2cb55b9a8e0e690e811515eb58
Giselle van Dongen <giselle.vd@gmail.com>
2024-10-18 11:46:44 +0200

    part2

--- java-hello-world-gradle/src/main/java/my/example/CartObject.java
+++ java-hello-world-gradle/src/main/java/my/example/CartObject.java
@@ -1,15 +1,41 @@
 package my.example;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import dev.restate.sdk.JsonSerdes;
 import dev.restate.sdk.ObjectContext;
 import dev.restate.sdk.annotation.Handler;
 import dev.restate.sdk.annotation.VirtualObject;
+import dev.restate.sdk.common.StateKey;
+import dev.restate.sdk.serde.jackson.JacksonSerdes;
+
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.Set;
 
 @VirtualObject
 public class CartObject {
 
+    StateKey<Set<String>> TICKETS =
+            StateKey.of("tickets",
+                    JacksonSerdes.of(new TypeReference<Set<String>>() {}));
+
     @Handler
     public boolean addTicket(ObjectContext ctx, String ticketId){
-        return true;
+
+        var success = TicketObjectClient.fromContext(ctx, ticketId)
+                .reserve().await();
+
+        if(success){
+            var tickets = ctx.get(TICKETS).orElse(new HashSet<>());
+            tickets.add(ticketId);
+            ctx.set(TICKETS, tickets);
+
+            CartObjectClient.fromContext(ctx, ctx.key())
+                    .send(Duration.ofMinutes(15))
+                    .expireTicket(ticketId);
+        }
+
+        return success;
     }
 
     @Handler
@@ -19,6 +45,32 @@ public class CartObject {
 
     @Handler
     public boolean checkout(ObjectContext ctx){
+        var tickets = ctx.get(TICKETS).orElse(new HashSet<>());
+
+        if(tickets.isEmpty()){
+            return false;
+        }
+
+        var payID = ctx.random().nextUUID().toString();
+        boolean success = ctx.run("pay", JsonSerdes.BOOLEAN,
+                () -> pay(payID, tickets.size()*40));
+
+        if(success){
+            for (String ticket : tickets) {
+                TicketObjectClient.fromContext(ctx, ticket)
+                        .send()
+                        .markAsSold();
+            }
+
+            ctx.clear(TICKETS);
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean pay(String idempotencyKey, double totalPrice){
+        System.out.println("Paying tickets for " + idempotencyKey + " and price " + totalPrice);
         return true;
     }
 }
--- java-hello-world-gradle/src/main/java/my/example/TicketObject.java
+++ java-hello-world-gradle/src/main/java/my/example/TicketObject.java
@@ -1,25 +1,47 @@
 package my.example;
 
+import dev.restate.sdk.JsonSerdes;
 import dev.restate.sdk.ObjectContext;
 import dev.restate.sdk.annotation.Handler;
 import dev.restate.sdk.annotation.VirtualObject;
+import dev.restate.sdk.common.StateKey;
 
 @VirtualObject
 public class TicketObject {
 
+    StateKey<String> STATUS =
+            StateKey.of("status", JsonSerdes.STRING);
+
     @Handler
     public boolean reserve(ObjectContext ctx){
-        return true;
+        var status = ctx.get(STATUS).orElse("Available");
+
+        if(status.equals("Available")){
+            ctx.set(STATUS, "Reserved");
+            return true;
+        }
+
+        return false;
     }
 
 
     @Handler
     public void unreserve(ObjectContext ctx){
+        var status = ctx.get(STATUS).orElse("Available");
+
+        if(!status.equals("Sold")){
+            ctx.set(STATUS, "Available");
+        }
     }
 
 
     @Handler
     public void markAsSold(ObjectContext ctx){
+        var status = ctx.get(STATUS).orElse("Available");
+
+        if(status.equals("Reserved")){
+            ctx.set(STATUS, "Sold");
+        }
     }
 
 }
